eaa8d5387aefd4262f29851afa15298528adc4e7
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-07-26 23:53:36 -0500

    fixed algorithm NR

--- NewtonRaphson.py
+++ NewtonRaphson.py
@@ -3,7 +3,8 @@
 __author__ = "Gonzalo Chacaltana"
 
 import sympy as sy
-import sys,os
+import sys
+import os
 
 
 class NewtonRaphson(object):
@@ -15,14 +16,18 @@ class NewtonRaphson(object):
 
     def set_options(self):
         self.rounding = self.options['rounding'] if self.options['rounding'] else 2
-        self._x = self.options['initial_root'] if self.options['initial_root'] else 0
+        self._x = self.options['initial_temp'] if self.options['initial_temp'] else 0
         self.max_error = self.options['max_error'] if self.options['max_error'] else 0.5
 
     def run(self):
         self.set_options()
         os.system('clear')
         print("\nDada la funcion f(x)")
-        print("\nf(x) = {}".format(self.fx))
+        print("\nf(x) = {}".format(sy.simplify(self.fx)))
+        print("\nDonde: x = T = Temperatura de la mezcla")
+        print("\n------------------------------------------")
+        print("Aplicando el Metodo de Newton-Raphson")
+        print("------------------------------------------")
         while(self.iterations < self.max_iterations):
             self.iterations = self.iterations+1
             self.calculate_root()
@@ -31,15 +36,15 @@ class NewtonRaphson(object):
 
     def calculate_root(self):
         print("\nIteracion {}".format(self.iterations))
-        print("-------------------")
-        print("Estimacion de la raiz, cuando x = {}".format(self._x))
+        print("---------------")
+        print("Cuando x = {}".format(self._x))
         self._y = round(self._x-(self.fx.subs(self.x, self._x) /
                                  self.df().subs(self.x, self._x)), self.rounding)
-        print("Obtenemos: y = {}".format(self._y))
+        print("Obtenemos: f(x) = f({}) = {}".format(self._x,self._y))
 
     def calculate_error(self):
-        self.error = (abs((self._y - self._x)/self._y))*100
-        print("Error absoluto: {}".format(str(self.error)+" %"))
+        self.error = round((abs((self._y - self._x)/self._y))*100,self.rounding)
+        print("Error absoluto relativo: {}".format(str(self.error)+" %"))
 
     def evaluate(self):
         if (self.error < self.max_error):
